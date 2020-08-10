f2636b20286d73597f6426448b4444701729c0e8
liguoliang <2313458226@qq.com>
2020-08-10 16:57:00 +0800

    "这是一个springboot测试项目"

---
+++ java_spring_boot/src/main/java/com/hqyj/javaSpringBoot/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.hqyj.javaSpringBoot.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/testDesc")
+    @ResponseBody
+    public String testDesc(){
+        return "lgl=======";
+    }
+}
